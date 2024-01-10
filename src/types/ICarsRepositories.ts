1ff4100cbaaa3212fa81b9f460b10740b417b8e4
Luiz D O Santos <lzdavi1325@gmail.com>
2024-01-10 19:26:13 -0300

    ajuste no tipo ICarsRepositories

--- src/types/ICarsRepositories.ts
+++ src/types/ICarsRepositories.ts
@@ -1,13 +1,13 @@
 import { Car } from "./Car";
 
 export interface ICarsRepositories {
-  findAll(): Promise<Car>;
+  findAll(): Promise<Car[]>;
 
   findOne(id: number): Promise<Car>;
 
   create(car: Car): Promise<Car>;
 
-  update(car: Partial<Car>): Promise<Car>;
+  update(car: Partial<Car>, id: number): Promise<Car>;
 
   remove(id: number): Promise<void>;
 }
